241c6ee755c4cb732b09ad83ea17819d6094c2a1
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-05-12 15:26:37 -0500

    Add tests for query log formatter.

---
+++ activerecord/test/cases/query_logs_formatter_test.rb
@@ -0,0 +1,25 @@
+require "cases/helper"
+
+class QueryLogsFormatter < ActiveRecord::TestCase
+def test_factory_invalid_formatter
+    assert_raises(ArgumentError) do
+      ActiveRecord::QueryLogs::FormatterFactory.from_symbol(:non_existing_formatter)
+    end
+  end
+
+  def test_factory_invalid_quote_values
+    assert_raises(ArgumentError) do
+      ActiveRecord::QueryLogs::Formatter.new(key_value_separator: ':', quote_values: :does_not_exist)
+    end
+  end
+
+  def test_sqlcommenter_key_value_separator
+    formatter = ActiveRecord::QueryLogs::FormatterFactory.from_symbol(:sqlcommenter)
+    assert_equal('=', formatter.key_value_separator)
+  end
+
+  def test_sqlcommenter_quote_value
+    formatter = ActiveRecord::QueryLogs::FormatterFactory.from_symbol(:sqlcommenter)
+    assert_equal("'Joe\\'s Crab Shack'", formatter.quote_value("Joe's Crab Shack"))
+  end
+end
