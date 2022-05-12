994e80b7d819aa07fdb810a14079e69f428a492f
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-05-12 15:33:10 -0500

    Rubocop.

--- activerecord/test/cases/query_logs_formatter_test.rb
+++ activerecord/test/cases/query_logs_formatter_test.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 require "cases/helper"
 
 class QueryLogsFormatter < ActiveRecord::TestCase
-def test_factory_invalid_formatter
+  def test_factory_invalid_formatter
     assert_raises(ArgumentError) do
       ActiveRecord::QueryLogs::FormatterFactory.from_symbol(:non_existing_formatter)
     end
-  end
+    end
 
   def test_factory_invalid_quote_values
     assert_raises(ArgumentError) do
-      ActiveRecord::QueryLogs::Formatter.new(key_value_separator: ':', quote_values: :does_not_exist)
+      ActiveRecord::QueryLogs::Formatter.new(key_value_separator: ":", quote_values: :does_not_exist)
     end
   end
 
   def test_sqlcommenter_key_value_separator
     formatter = ActiveRecord::QueryLogs::FormatterFactory.from_symbol(:sqlcommenter)
-    assert_equal('=', formatter.key_value_separator)
+    assert_equal("=", formatter.key_value_separator)
   end
 
   def test_sqlcommenter_quote_value
