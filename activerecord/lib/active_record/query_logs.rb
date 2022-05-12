240d324f6603df4cfe59ac3b178e39f77a54f9df
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-05-12 17:37:28 -0500

    Add back in require relative.

--- activerecord/lib/active_record/query_logs.rb
+++ activerecord/lib/active_record/query_logs.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "active_support/core_ext/module/attribute_accessors_per_thread"
+require_relative "query_logs_formatter"
 
 module ActiveRecord
   # = Active Record Query Logs
